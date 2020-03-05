6c65685c62a545167c43dcec73a2f3b062770852
Brian Moy <bsavold@gmail.com>
2020-03-05 12:18:24 -0700

    output in dom

--- digital-clock.js
+++ digital-clock.js
@@ -1,1 +1,20 @@
 var timeToday = new Date();
+var getMinutes = timeToday.getMinutes();
+var getHour = timeToday.getHours();
+var getSeconds = timeToday.getSeconds();
+var addZero = "0";
+
+timeToday = getHour + ":" + getMinutes + ":" + getSeconds;
+
+console.log(timeToday);
+
+
+/*
+//Adding leading zero
+if (getSeconds < 10) {
+    addZero = addZero + getSeconds;
+}
+else{
+
+}
+*/
