a90f01102a1e4492f75ff5f601b98a545c2d9926
Brian Moy <bsavold@gmail.com>
2020-03-10 09:35:51 -0600

    Added logic, took out local attribute, added single attributes

--- app.js
+++ app.js
@@ -1,30 +1,68 @@
-var timeToday = new Date();
-var getMinutes = timeToday.getMinutes();
-var getHour = timeToday.getHours();
-var getSeconds = timeToday.getSeconds();
-//var addZero = "0";
+var interval = setInterval(callTimeDate, 1000);
+//var changeTimeButton = document.getElementById("button");
 
-function callTime(){
 
-    timeToday = getHour + ":" + getMinutes + ":" + getSeconds;
-    console.log(timeToday);
+
+
+
+function callTimeDate(){
+    var timeDateToday = new Date();
+    var showHours = timeDateToday.getHours();
+    var showMinutes = timeDateToday.getMinutes();
+    var showSeconds = timeDateToday.getSeconds();
+    //var getAmOrPm = ;
+    var showTime = showHours + ":" + showMinutes + ":" + showSeconds
+
+
+    showSeconds = showSeconds > 10 ? "0"+showSeconds : showSeconds;
+    
+    //Hours convert to military
+    if (showHours % 12) {
+        showHours = showHours;
+    }else{
+        showHours = 12;
+    }
+    //adding zero to minutes
+    if (showMinutes < 10) {
+        showMinutes = "0" + showMinutes;
+    } else {
+        showMinutes;
+    }
+    /*
+    //adding zero to seconds
+    if (showSeconds < 10) {
+        showSeconds = "0" + showSeconds;
+    }else{
+        showSeconds = showSeconds;
+    }
+    */
+    
+    return showTime;
+
+    // Use Ternary Operator
+    
+    //getHours = getHours ? getHours : 12;
+    
+
+    
+    //document.getElementById("time").innerHTML = showTime;
 }
-
-setInterval(callTime, 1000);
-
+console.log(callTimeDate());
 
 
 
 
-document.write(timeToday);
 
 
-/*
-//Adding leading zero
-if (getSeconds < 10) {
-    addZero = addZero + getSeconds;
-}
-else{
 
+/* Leaving for idea of creating function for 24 hour time
+function changeTime() {
+    
+
+    var changeTime = new Date();
+    var changeToMilitaryTime = changeTime.getHours() + ":" + changeTime.getMinutes() + ":" + changeTime.getSeconds();
+    
+    document.getElementById("time").innerHTML = changeToMilitaryTime;
 }
 */
+
