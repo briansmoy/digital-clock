871ab99977e20c4873ec9ca83523991dae7fa5c1
Brian Savold <m0rph1c@Brians-MacBook-Pro.local>
2020-03-14 08:01:00 -0600

    updated 12 hour clock

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 var interval = setInterval(callTimeDate, 1000);
 //var changeTimeButton = document.getElementById("button");
 
+/* if statement for onclick
+
+if onclick = true{
+    setInterval(callTimeDate)
+}else{
+    figure out later
+}
+
+/* function for 12 hour clock*/
+
+function callTwelveTime() {
+    var timeTwelveToday = new Date();
+    var getTwelveHours = timeTwelveToday.getHours();
+    var getTwelveMinutes = timeTwelveToday.getMinutes();
+    var getTwelveSeconds = timeTwelveToday.getSeconds();
+
+    //convert hours from 24 to 12
+    getTwelveHours = (getTwelveHours % 12) || 12;
+
+    //display 12 when 24%12
+    if (getTwelveHours % 12) {
+        getTwelveHours = getTwelveHours % 12;
+    } else {
+        getTwelveHours = 12;
+    }
+
+    if(getTwelveMinutes < 10) {
+        getTwelveMinutes = "0" + getTwelveMinutes;
+    }else{
+
+    }
+
+    if(getTwelveSeconds < 10) {
+        getTwelveSeconds = "0" + getTwelveSeconds;
+    }else{
+
+    }
+
+    document.getElementById("time").innerHTML = getTwelveHours + ":" + getTwelveMinutes + ":" + getTwelveSeconds;
+}
+
 function callTimeDate(){
     var timeDateToday = new Date();
     var showHours = timeDateToday.getHours();
@@ -25,20 +66,3 @@ function callTimeDate(){
     //document.getElementById("date").innerHTML = showDate;
 
 }
-
-
-
-
-
-/*Work on later - Convert 12 hour format to 24
-
-//convert hours from 24 to 12
-showHours = (showHours % 12) || 12;
-
-//display 12 when 24%12
-    if (showHours % 12) {
-        showHours = showHours % 12;
-    } else {
-        showHours = 12;
-    }
-*/
